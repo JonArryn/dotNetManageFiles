0a3906ae165baa88a695ee804e5eaf5c4b15b309
Jonathan <jonathan@Apple-Crisp.local>
2023-04-12 11:02:38 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+// Code
+var currentDirectory = Directory.GetCurrentDirectory();
+
+// Reference variables for directories
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+
+// Creates salesTotalDir directory
+Directory.CreateDirectory(salesTotalDir);
+
+// Executes method to find files in the storesDirectory
+var salesFiles = FindFiles(storesDirectory);
+
+// Get total sales number
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+// Creates a new text file 'totals.txt' with an empty string
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+// Find files method
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData(double Total);
